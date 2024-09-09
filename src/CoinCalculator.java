39df453ba291f1d195fd834d2ecdd84a72eb96c1
kyoohyunlee <khyhlee@naver.com>
2024-09-09 22:08:08 +0900

    2주차 실습

---
+++ src/CoinCalculator.java
@@ -0,0 +1,13 @@
+public class CoinCalculator {
+    public static void main(String[] args) {
+        int quarters = 9;
+        int dimes = 2;
+        int nickels = 0;
+        int pennies = 6;
+        int sum = quarters*25+dimes*10+nickels*5+pennies*1;
+        int dollar = sum/100;
+        int cent = sum%100;
+        System.out.println("$"+ dollar+"."+cent);
+        // 문자 이어붙이기 + 연산자 중복 사용 overloading
+    }
+}
--- src/DivisibleBy9.java
+++ src/DivisibleBy9.java
@@ -6,7 +6,5 @@ public class DivisibleBy9 {
         String input = JOptionPane.showInputDialog(message);
         boolean result = Integer.parseInt(input)%9 ==0;
         JOptionPane.showMessageDialog(null, "정수 "+input+" 은(는) 9로 나누어 떨어지나?"+result);
-
-
     }
 }
