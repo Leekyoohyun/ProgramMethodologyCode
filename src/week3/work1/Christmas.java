7f96ea485211c5e7f0c1bab56e0e76d1f773c41e
kyoohyunlee <khyhlee@naver.com>
2024-09-23 22:06:09 +0900

    DayCalc --> Christmas 수정 일단 첫번째 작업 끝

--- src/week3/work1/Christmas.java
+++ src/week3/work1/Christmas.java
@@ -2,21 +2,18 @@ package week3.work1;
 
 import javax.swing.*;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.Period;
 
-public class DayCalc {
+public class Christmas {
     public static void main(String[] args) {
         LocalDate today = LocalDate.now();
         LocalDate christmas = LocalDate.of(2024,12,25);
-        String in = JOptionPane.showInputDialog(null);
+        JOptionPane.showMessageDialog(null, "오늘은 "+today);
 
         int monthResult = Period.between(today, christmas).getMonths();
         int dayResult = Period.between(today, christmas).getDays();
 
-        System.out.println(monthResult+dayResult);
-
-        //String out = JOptionPane.showMessageDialog(null, "올해 크리스마스까지 "+monthResult+" 달하고"+dayResult+" 일 남았다!");
+        JOptionPane.showMessageDialog(null, "올해 크리스마스까지 "+monthResult+" 달하고"+dayResult+" 일 남았다!");
 
     }
 }
