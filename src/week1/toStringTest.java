cb09877d83f43f85b561a9fa0f5387ad003374a7
kyoohyunlee <khyhlee@naver.com>
2024-09-30 11:32:34 +0900

    week1에 toString 테스트

---
+++ src/week1/toStringTest.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class toStringTest {
+    public static void main(String[] args) {
+        int i = 1111;
+        String newS = Integer.toString(i);
+        System.out.println(newS);
+    }
+}
