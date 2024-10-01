ceda3f75bc34a752f021a1b531a014f0db2c2c6e
kyoohyunlee <khyhlee@naver.com>
2024-10-01 18:45:42 +0900

    Add "Jude" on canvas& size Up

--- src/week4/writeOnCanvas/Canvas.java
+++ src/week4/writeOnCanvas/Canvas.java
@@ -8,15 +8,17 @@ public class Canvas extends JPanel {
     public Canvas() {
         JFrame frame = new JFrame();
         frame.setTitle("Canvas");
-        frame.setSize(300,200);
+        frame.setSize(700,500);
         frame.getContentPane().add(this); //객체 자신을 가리키는 이름
-        frame.setVisible(true);
+        frame.setVisible(true); //프레임을 화면에 보이도록 설정합니다. 이 코드가 호출되지 않으면 GUI 창이 보이지 않습니다.
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     public void paintComponent(Graphics g) {
         g.setColor(Color.red);
         g.drawString("HEY",100,60);
+        g.setColor(Color.blue);
+        g.drawString("Jude",200,60);
     }
 
     public static void main(String[] args) {
