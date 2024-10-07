c27ef7190847cdfc1794fad46129d238eb33bf6a
kyoohyunlee <khyhlee@naver.com>
2024-10-07 16:51:18 +0900

    Make GrowingEgg class

---
+++ src/week5/work1/GrowingEgg.java
@@ -0,0 +1,14 @@
+package week5.work1;
+
+import javax.swing.*;
+
+public class GrowingEgg extends JPanel {
+    public GrowingEgg() {
+        JFrame frame = new JFrame();
+        frame.setTitle("Growing Egg");
+    }
+
+    public static void main(String[] args) {
+        new GrowingEgg();
+    }
+}
