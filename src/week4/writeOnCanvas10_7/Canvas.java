33a44fba756a38b0c9376fb4082c5ce363863ca4
kyoohyunlee <khyhlee@naver.com>
2024-10-07 11:50:17 +0900

    Add Canvas 10/07class

---
+++ src/week4/writeOnCanvas10_7/Canvas.java
@@ -0,0 +1,30 @@
+package week4.writeOnCanvas10_7;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Canvas extends JPanel {
+    public Canvas() {
+        JFrame frame = new JFrame();
+        frame.setTitle("Canvas");
+        frame.setSize(300,200);
+        frame.getContentPane().add(this); // 자기자신(this)를 Canvas에 집어넣어라
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        //super.paintComponent(g); 상속받았다?
+        g.setColor(Color.BLACK);
+        g.drawString("Java!", 100, 60);
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String[] args){
+        Canvas c = new Canvas();
+    }
+}
