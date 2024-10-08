3df17bbf72d74d3080d0a45dd0419f34de50b7e4
kyoohyunlee <khyhlee@naver.com>
2024-10-08 17:13:46 +0900

    Math Library test

---
+++ src/week2/mathLib/MathLibEx1.java
@@ -0,0 +1,11 @@
+package week2.mathLib;
+
+public class MathLibEx1 {
+    public static void main(String[] args) {
+        double r = 3.14;
+
+        System.out.println(Math.sqrt(r));   // 루트씌우기
+        System.out.println(Math.pow(r,2));  // 제곱
+        System.out.println(Math.abs(-r));   // 절댓값
+    }
+}
