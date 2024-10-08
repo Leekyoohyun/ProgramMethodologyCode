3f1554941bbe46b212fef096f5ddd2bcb1d1cdad
kyoohyunlee <khyhlee@naver.com>
2024-10-08 17:38:23 +0900

    Add Slogan.java

---
+++ src/week2/mathLib/Slogan.java
@@ -0,0 +1,11 @@
+package week2.mathLib;
+
+public class Slogan {
+    public static void main(String[] args) {
+        int i = 1;
+        double d = 2.4999999;
+        int sum = (int)(i+d);
+        System.out.println(sum);
+
+    }
+}
