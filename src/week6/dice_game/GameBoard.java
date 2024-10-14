f378aab341f57a46243dba318d8d2a9ccdb29935
kyoohyunlee <khyhlee@naver.com>
2024-10-14 23:38:05 +0900

    Fix GameBoard.java View Class

--- src/week6/dice_game/GameBoard.java
+++ src/week6/dice_game/GameBoard.java
@@ -5,8 +5,8 @@ import java.awt.*;
 
 public class GameBoard extends JPanel {
 
-    private int WIDTH = 520;
-    private int HEIGHT = 520;
+    private int WIDTH = 500;
+    private int HEIGHT = 500;
     private Player player1;
     private Player player2;
 
@@ -35,27 +35,30 @@ public class GameBoard extends JPanel {
 
         // 플레이어 1 정보 출력
         g.setColor(Color.WHITE);
-        g.drawString(player1.name(), 140 ,140);
-        g.drawString("누적점수: " + player1.points(), 140, 180);
+        g.drawString(player1.name(), 110 ,180);
+        g.drawString("누적점수: " + player1.points(), 110, 220);
         if (player1.rolled() != null) {
-            g.drawString(player1.rolled().face1() + ", " + player1.rolled().face2(), 140, 260);
+            g.drawString(player1.rolled().face1() + ", " + player1.rolled().face2(), 110, 300);
         }
 
         // 플레이어 2 정보 출력
-        g.drawString(player2.name(), 370, 140);
-        g.drawString("누적점수: " + player2.points(), 370, 180);
+        g.drawString(player2.name(), 350, 180);
+        g.drawString("누적점수: " + player2.points(), 350, 220);
         if (player2.rolled() != null) {
-            g.drawString(player2.rolled().face1() + ", " + player2.rolled().face2(), 370, 260);
+            g.drawString(player2.rolled().face1() + ", " + player2.rolled().face2(), 350, 300);
         }
 
         // 결과 출력
         if (player1.rolled() != null && player2.rolled() != null) {
             if (player1.wins()) {
-                g.drawString("이겼다!", 140, 220);
+                g.setColor(Color.RED);
+                g.drawString("이겼다!", 110, 260);
             } else if (player2.wins()) {
-                g.drawString("이겼다!", 370, 220);
+                g.setColor(Color.BLUE);
+                g.drawString("이겼다!", 350, 260);
             } else {
-                g.drawString("비겼다!", 235, 220);
+                g.setColor(Color.YELLOW);
+                g.drawString("비겼다!", 240, 260);
             }
         }
     }
