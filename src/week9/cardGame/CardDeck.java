863707e47cc37eb3630cb19fcb14b10734ed98ac
kyoohyunlee <khyhlee@naver.com>
2024-11-08 22:54:41 +0900

    dev: fixing cardgame2

--- src/week9/cardGame/CardDeck.java
+++ src/week9/cardGame/CardDeck.java
@@ -1,9 +1,25 @@
 package week9.cardGame;
 
 public class CardDeck {
-    Card[] buildDeck(){
-        Card[] deck = new Card[52];
+    private Card[] deck = new Card[4*Card.SIZE_OF_ONE_SUIT]; // 카드 덱
+    private int card_count; // 덱에 남아있는 카드 장수
+    // deck[0], ... , deck[card_count_1] 에 카드가 있음
 
-        return deck;
+    // CardDeck 카드 한 벌 생성
+    public CardDeck(){
+        createDeck();
+    }
+
+    private void createDeck(){
+        createSuit(Card.SPADES);
+        createSuit(Card.HEARTS);
+        createSuit(Card.CLUBS);
+        createSuit(Card.DIAMONDS);
+    }
+
+    private void createSuit(String which_suit){
+        for(int i = 1; i<=Card.SIZE_OF_ONE_SUIT; i++){
+            deck[card_count]= new Card(which_suit,i);
+        }
     }
 }
