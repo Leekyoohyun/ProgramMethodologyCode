3330147966cc93a8ddddc439bb810ad3139abf9b
kyoohyunlee <khyhlee@naver.com>
2024-12-08 18:31:47 +0900

    dev: tester

--- src/java_final_1st/file_in_out/exceptionEx/TestDialogReader.java
+++ src/java_final_1st/file_in_out/exceptionEx/TestDialogReader.java
@@ -1,9 +1,16 @@
 package java_final_1st.file_in_out.exceptionEx;
 
+import javax.swing.*;
+import java.lang.ArithmeticException;
+
 public class TestDialogReader {
     public static void main(String[] args) {
-        DialogReader reader = new DialogReader();
-
-        reader.readDouble("정수?");
+        int n = new DialogReader().readInt("정수 줘");
+        try{
+            String answer = "100 / "+ Integer.toString(n)+" = "+(100/n);
+            JOptionPane.showMessageDialog(null, answer);
+        }catch (ArithmeticException a){
+            JOptionPane.showMessageDialog(null, a.getMessage()+"0 으로 못나눔");
+        }
     }
 }
